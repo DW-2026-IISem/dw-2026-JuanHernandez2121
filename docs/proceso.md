5e99072b813d16c106c8a03767807c4be182e242
Juan Hernandez <juanperal2180@gmail.com>
2026-09-13 02:39:22 -0500

    .26 — Actualizar business.module.ts

--- docs/proceso.md
+++ docs/proceso.md
@@ -855,3 +855,7 @@ mkdir -p /home/portatiljq/apps/dlloweb/nestjs/express_sequelize chmod -R 755 /ho
 #### **.25 — Actualizar sequelize.factory.ts (registrar modelos)**
 
 ![](images/clipboard-2251047456.png)
+
+#### **.26 — Actualizar business.module.ts**
+
+![](images/clipboard-2288635362.png)
--- src/features/business/business.module.ts
+++ src/features/business/business.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
+import { PlansModule } from './plans/plans.module';
 
 @Module({
-  imports: [ClientsModule],
-  exports: [ClientsModule],
+  imports: [ClientsModule, PlansModule],
+  exports: [ClientsModule, PlansModule],
 })
 export class BusinessModule {}
