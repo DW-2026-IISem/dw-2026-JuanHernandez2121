74895298ff651ead654927c574d1fde75f5ad08d
Juan Hernandez <juanperal2180@gmail.com>
2026-09-12 23:44:28 -0500

    9.27 — Actualizar business.module.ts

--- src/features/business/business.module.ts
+++ src/features/business/business.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 
 import { ClientsModule } from './clients/clients.module.js';
-
 import { ProductTypesModule } from './product-types/product-types.module.js';
+import { ProductsModule } from './products/products.module.js';
 
 @Module({
-  imports: [ClientsModule, ProductTypesModule],
-  exports: [ClientsModule, ProductTypesModule],
+  imports: [
+    ClientsModule,
+    ProductTypesModule,
+    ProductsModule,
+  ],
+  exports: [
+    ClientsModule,
+    ProductTypesModule,
+    ProductsModule,
+  ],
 })
 export class BusinessModule {}
